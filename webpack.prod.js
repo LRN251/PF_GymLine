8b721b4e6d8be5a4ee2faedd681e235b0abffac8
Nanit20 <55482552+Nanit20@users.noreply.github.com>
2024-05-24 03:44:50 +0000

    prueba10

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,52 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: '/'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
+
+module.exports = {
+  entry: [
+    './src/front/js/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Cambia 'public' a 'dist'
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/, use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }]
+      }, // css only files
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/', // Guarda las imágenes en una carpeta llamada 'images'
+            publicPath: '/images/' // Ruta pública para acceder a las imágenes
+          }
+        }
+      }, // for images
+      { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } // for fonts
     ]
-});
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: '4geeks.ico',
+      template: 'template.html'
+    }),
+    new Dotenv({ safe: true, systemvars: true })
+  ]
+};
